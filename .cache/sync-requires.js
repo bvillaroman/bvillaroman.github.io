fcc695ce81459c3ae9fb2b284e8300453ddea869
Bruce <bvillaroman@gmail.com>
2017-10-05 17:11:46 -0400

    8-5-17

---
+++ .cache/sync-requires.js
@@ -0,0 +1,30 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/bruce/bvillaroman/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/bruce/bvillaroman/src/pages/404.js")),
+  "component---src-pages-portraits-js": preferDefault(require("/Users/bruce/bvillaroman/src/pages/Portraits.js")),
+  "component---src-pages-shoots-js": preferDefault(require("/Users/bruce/bvillaroman/src/pages/Shoots.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/bruce/bvillaroman/src/pages/index.js"))
+}
+
+exports.json = {
+  "layout-index.json": require("/Users/bruce/bvillaroman/.cache/json/layout-index.json"),
+  "dev-404-page.json": require("/Users/bruce/bvillaroman/.cache/json/dev-404-page.json"),
+  "layout-index.json": require("/Users/bruce/bvillaroman/.cache/json/layout-index.json"),
+  "404.json": require("/Users/bruce/bvillaroman/.cache/json/404.json"),
+  "layout-index.json": require("/Users/bruce/bvillaroman/.cache/json/layout-index.json"),
+  "portraits.json": require("/Users/bruce/bvillaroman/.cache/json/portraits.json"),
+  "layout-index.json": require("/Users/bruce/bvillaroman/.cache/json/layout-index.json"),
+  "shoots.json": require("/Users/bruce/bvillaroman/.cache/json/shoots.json"),
+  "layout-index.json": require("/Users/bruce/bvillaroman/.cache/json/layout-index.json"),
+  "index.json": require("/Users/bruce/bvillaroman/.cache/json/index.json"),
+  "layout-index.json": require("/Users/bruce/bvillaroman/.cache/json/layout-index.json"),
+  "404-html.json": require("/Users/bruce/bvillaroman/.cache/json/404-html.json")
+}
+
+exports.layouts = {
+  "component---src-layouts-index-js": preferDefault(require("/Users/bruce/bvillaroman/.cache/layouts/index.js"))
+}
