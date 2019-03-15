bb52d2fdb0cb7eb890f902d25b159b9c2bb6f963
Bruce <bvillaroman@gmail.com>
2019-03-15 12:54:42 -0400

    addded webpack fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,58 +7,13 @@ module.exports = {
     title : "bvillaroman",
   },
   plugins: [
-    // {
-    //   resolve: `gatsby-plugin-favicon`,
-    //   options: {
-    //       logo: "./src/favicon.png",
-    //       injectHTML: true,
-    //       icons: {
-    //         android: true,
-    //         appleIcon: true,
-    //         appleStartup: true,
-    //         coast: false,
-    //         favicons: true,
-    //         firefox: true,
-    //         twitter: false,
-    //         yandex: false,
-    //         windows: false
-    //       }
-    //   }
-    // },
-    // `gatsby-transformer-sharp`,
-    // `gatsby-plugin-sharp`,
     `gatsby-plugin-styled-components`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //   },
-    // },
-    `gatsby-plugin-stripe`,
-    // {
-    //   resolve: `gatsby-plugin-favicon`,
-    //   options: {
-    //       logo: "./src/favicon.png",
-    //       injectHTML: true,
-    //       icons: {
-    //         android: true,
-    //         appleIcon: true,
-    //         appleStartup: true,
-    //         coast: false,
-    //         favicons: true,
-    //         firefox: true,
-    //         twitter: false,
-    //         yandex: false,
-    //         windows: false
-    //       }
-    //   }
-    // },
+    {
+      resolve: `gatsby-plugin-stripe`,
+      options: {
+        async: true,
+      },
+    },
     {
       resolve: `gatsby-source-stripe`,
       options: {
@@ -67,8 +22,5 @@ module.exports = {
         downloadFiles: true
       }
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
   ],
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -3,20 +3,35 @@ import {Container,AboutContainer} from '../styles/containers.js'
 import CheckoutForm from "../components/checkoutForm"
 import {StripeProvider,Elements} from 'react-stripe-elements';
 
-export default (props) => {
-  return (
-    <StripeProvider apiKey={process.env.STRIPE_API}>
-      <Container>
-        <AboutContainer>
-          <h3>ABRAHAM BRUCE VILLAROMAN</h3>
-          <h3>BVILLAROMAN@GMAIL.COM</h3>
-          <h3>631-871-9578</h3>
-          <Elements>
-            <CheckoutForm />
-          </Elements>
-        </AboutContainer>
-      </Container>
-    </StripeProvider>
-  ) 
+class About extends React.Component{
+
+  componentDidMount() {
+    if (window.Stripe) {
+      this.setState({stripe: window.Stripe(process.env.STRIPE_API)});
+    } else {
+      document.querySelector('#stripe-js').addEventListener('load', () => {
+        // Create Stripe instance once Stripe.js loads
+        this.setState({stripe: window.Stripe(process.env.STRIPE_API)});
+      });
+    }
+  }
+
+  render() {
+    return (
+      <StripeProvider apiKey={process.env.STRIPE_API}>
+        <Container>
+          <AboutContainer>
+            <h3>ABRAHAM BRUCE VILLAROMAN</h3>
+            <h3>BVILLAROMAN@GMAIL.COM</h3>
+            <h3>631-871-9578</h3>
+            <Elements>
+              <CheckoutForm />
+            </Elements>
+          </AboutContainer>
+        </Container>
+      </StripeProvider>
+    ) 
+  }
 }
 
+export default About;
